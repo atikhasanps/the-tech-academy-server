c6daf54cf75d7a21fbb4de8936def3f73bd73e46
atikhasanps <atikhasanps@gmail.com>
2022-10-27 19:44:18 +0600

    final

--- index.js
+++ index.js
@@ -7,15 +7,31 @@ const port =process.env.PORT || 5000;
 app.use(cors());
 
 const courses = require('./data/courses.json');
+const courseCategories = require('./data/courseCategories.json');
 
 app.get('/', (req, res) =>{
     res.send('Courses API running')
 
 });
 
-app.get('/courses',(req,res) =>{
+app.get('/course-categories', (req, res)=>{
+    res.send(courseCategories)
+})
+
+app.get('/courses', (req, res) =>{
     res.send(courses)
 })
+app.get('/courses/:id',(req,res) =>{
+   const id = req.params.id;
+   const selectCourses = courses.find(n=> n.id ===id);
+   res.send(selectCourses);
+})
+app.get('/courseCategories/:id',(req,res) =>{
+    const id= req.params.id;
+    const category_courses =courseCategories.find( n=>n.id ===id)
+    res.send(category_courses)
+})
+
 
 app.listen(port, ()=>{
     console.log('tech academy server running on port', port)
